8d51a23fb3418070a4f94e8bac0a157be3c52e73
Luke Towers <git@luketowers.ca>
2024-02-28 21:24:22 -0600

    Add listAccounts() method

--- src/GoogleAds.php
+++ src/GoogleAds.php
@@ -4,8 +4,11 @@ namespace LukeTowers\LaravelGoogleAds;
 
 use Google\Ads\GoogleAds\Lib\Configuration;
 use Google\Ads\GoogleAds\Lib\OAuth2TokenBuilder;
-use Google\Ads\GoogleAds\Lib\V16\GoogleAdsClientBuilder;
 use Google\Ads\GoogleAds\Lib\V16\GoogleAdsClient;
+use Google\Ads\GoogleAds\Lib\V16\GoogleAdsClientBuilder;
+use Google\Ads\GoogleAds\V16\Services\ListAccessibleCustomersRequest;
+use Google\Ads\GoogleAds\V16\Services\SearchGoogleAdsRequest;
+use Google\ApiCore\ApiException;
 use Google\Auth\FetchAuthTokenInterface;
 
 class GoogleAds
@@ -31,5 +34,50 @@ class GoogleAds
             ->from($this->config)
             ->withOAuth2Credential($this->oAuth2Credential)
             ->build();
+    }
+
+    /**
+     * Get the underlying GoogleAdsClient instance
+     */
+    public function getClient(): GoogleAdsClient
+    {
+        return $this->client;
+    }
+
+    /**
+     * List the Customer accounts available to the current instance
+     *
+     * This will be affected by the OAuth2 credentials used to authenticate
+     * and the developer token used to build the client
+     */
+    public function listCustomers(): array
+    {
+        $customers = [];
+
+        $client = $this->getClient();
+
+        $customerServiceClient = $client->getCustomerServiceClient();
+        $response = $customerServiceClient->listAccessibleCustomers(new ListAccessibleCustomersRequest());
+        $accessibleCustomers = $response->getResourceNames();
+
+        foreach ($accessibleCustomers as $customerResourceName) {
+            $customerId = str_replace('customers/', '', $customerResourceName);
+            $query = "SELECT customer.descriptive_name FROM customer WHERE customer.id = $customerId";
+            $request = SearchGoogleAdsRequest::build($customerId, $query);
+
+            try {
+                $response = $client->getGoogleAdsServiceClient()->search($request);
+            } catch (ApiException $ex) {
+                continue;
+            }
+
+            foreach ($response->iterateAllElements() as $googleAdsRow) {
+                $customer = $googleAdsRow->getCustomer();
+                $descriptiveName = $customer->getDescriptiveName();
+                $customers[$customerId] = $descriptiveName;
+            }
+        }
+
+        return $customers;
     }
 }
